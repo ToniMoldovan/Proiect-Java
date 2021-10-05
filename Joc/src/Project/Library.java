ac5fbfc24e28be27631d16829e1903474c748374
Toni <antonio.moldovan82@yahoo.com>
2021-10-05 23:54:59 +0300

    Added create book function

--- Joc/src/Project/Library.java
+++ Joc/src/Project/Library.java
@@ -16,6 +16,7 @@ public class Library {
         String bookCategory;
         int bookPagesNumber;
         int belongsTo;
+        private int _bookId = booksInLibrary + 1;
         boolean bookPremium;
 
         Book next;
@@ -112,13 +113,13 @@ public class Library {
         System.out.println("All books in the library (" + numberOfBooks + ")");
         while(current != null) {
             //Prints each node by incrementing pointer
-            System.out.println("Book [" + current.bookName + "] Author [" + current.bookAuthor + "] Category [" + current.bookCategory + "] No. pages [" + current.bookPagesNumber + "] Is Premium [" + current.bookPremium + "] Belongs to [" + current.belongsTo + "].");
+            System.out.println("Book ID [" + current._bookId + "] Name [" + current.bookName + "] Author [" + current.bookAuthor + "] Category [" + current.bookCategory + "] No. pages [" + current.bookPagesNumber + "] Is Premium [" + current.bookPremium + "] Belongs to [" + current.belongsTo + "].");
             current = current.next;
         }
         System.out.println("\n");
     }
 
-    public void showBooksById(int id) {
+    public void showBooksPersonById(int id) {
         Book current = head;
         int numberOfBooks = 0;
 
@@ -138,11 +139,92 @@ public class Library {
 
     }
 
-    public void setAllBooksByIdOnZero(int id) { //for when you want to delete a user, set all his books on zero
+    public void addBookToPersonLibraryById(int id) {
+        Book current = head;
+        System.out.println("Input the book ID you want to add: ");
+        Scanner sc = new Scanner(System.in);
+        int bookId = sc.nextInt();
 
+        while (current != null) {
+            if (current._bookId == bookId && current.belongsTo == 0) {
+                current.belongsTo = id;
+            }
+            else if (current.belongsTo != 0) {
+                System.out.println("This book belongs to [" + current.bookAuthor + "].");
+            }
+            current = current.next;
+        }
     }
+
+    public void removeBookById(int position) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter admin password: ");
+        String adminPw = sc.nextLine();
+
+        int booksNumber = getBooksInLibrary();
+
+        if (adminPw.equals("pufuleti")) {
+            position = position - 1;
+            // If linked list is empty
+            if (head == null)
+                return;
+
+            // Store head node
+            Book temp = head;
+
+            // If head needs to be removed
+            if (position == 0)
+            {
+                head = temp.next;   // Change head
+                booksNumber = booksNumber - 1;
+                setBooksInLibrary(booksNumber);
+                return;
+            }
+
+            // Find previous node of the node to be deleted
+            for (int i=0; temp!=null && i<position-1; i++)
+                temp = temp.next;
+
+            // If position is more than number of nodes
+            if (temp == null || temp.next == null)
+                return;
+
+            // Node temp->next is the node to be deleted
+            // Store pointer to the next of node to be deleted
+            booksNumber = booksNumber - 1;
+            setBooksInLibrary(booksNumber);
+            Book next = temp.next.next;
+
+            temp.next = next;  // Unlink the deleted node from list
+        }
+        else {
+            System.out.println("The password is incorrect.");
+            return;
+        }
+    }
+
+    public void setBookByIdOnZero() { //for when you want to delete a user, set all his books on zero
+        Book current = head;
+        System.out.println("Input the book ID you want to set on zero: ");
+        Scanner sc = new Scanner(System.in);
+        int bookId = sc.nextInt();
+
+        while (current != null) {
+            if (current._bookId == bookId && current.belongsTo != 0) {
+                current.belongsTo = 0;
+            }
+            else if (current.belongsTo == 0) {
+                System.out.println("This book already belongs to nobody.");
+            }
+            current = current.next;
+        }
+    }
+
     public int getBooksInLibrary() {
         return booksInLibrary;
     }
 
+    public void setBooksInLibrary(int booksInLibrary) {
+        this.booksInLibrary = booksInLibrary;
+    }
 }
--- Joc/src/Project/Main.java
+++ Joc/src/Project/Main.java
@@ -7,6 +7,10 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         People person = new People();
+        person.library.addNewBookInLibrary("test1", "toni", "comedy", 200, true, 0);
+        person.library.addNewBookInLibrary("test2", "toni", "comedy", 200, true, 0);
+        person.library.addNewBookInLibrary("test3", "toni", "comedy", 200, true, 0);
+        person.library.addNewBookInLibraryUnkownAuthor("test4", "comedy", 20, false,  0);
         mainMenu(person);
 
     }
@@ -20,11 +24,11 @@ public class Main {
         System.out.println("------| General Library |-----");
         System.out.println("5) See all the books in the library");
         System.out.println("6) Add a book to the library");
-        System.out.println("7) Remove a book from the library");
+        System.out.println("7) Remove a book from the library by Id");
         System.out.println("------| People Library |----");
         System.out.println("8) See all books of a person by Id");
         System.out.println("9) Add a book to person by Id");
-        System.out.println("10) Remove a book to person by Id");
+        System.out.println("10) Remove a book from person by Id");
         System.out.println("----------------------------");
         System.out.println("11) Exit program");
 
@@ -41,7 +45,7 @@ public class Main {
         } else if (Objects.equals(response, "1")) {
             createPerson(person);
         } else if (Objects.equals(response, "2")) {
-            person.showAllPeople();
+            showAllPersons(person);
         } else if (Objects.equals(response, "3")) {
             banPerson(person);
         } else if (Objects.equals(response, "4")) {
@@ -51,13 +55,13 @@ public class Main {
         } else if (Objects.equals(response, "6")) {
             addBookToGeneralLibrary(person);
         } else if (Objects.equals(response, "7")) {
-            //createNewPerson();
+            removeBookById(person);
         } else if (Objects.equals(response, "8")) {
-            //createNewPerson();
+            showPersonLibrary(person);
         } else if (Objects.equals(response, "9")) {
-            //createNewPerson();
+            addBookToPersonLibrary(person);
         } else if (Objects.equals(response, "10")) {
-            //createNewPerson();
+            setBookOnZeroById(person);
         } else {
             System.out.println("I can't understand your answer. Please try again.\n");
             TimeUnit.SECONDS.sleep(2);
@@ -92,6 +96,11 @@ public class Main {
 
         //Creating person
         person.createNewPerson(personName, isAdmin);
+        mainMenu(person);
+    }
+
+    public static void showAllPersons(People person) throws InterruptedException {
+        person.showAllPeople();
         mainMenu(person);
     }
 
@@ -163,6 +172,7 @@ public class Main {
             System.out.println("Enter the book author: ");
             bookAuthor = sc.nextLine();
 
+            System.out.println("Available categories: (1) SF | (2) Adventure | (3) Programming | (4) Comedy");
             System.out.println("Enter the book category (1/2/3/4): ");
             bookCategory = sc.next();
 
@@ -216,6 +226,39 @@ public class Main {
 
     public static void showAllBooksInGeneralLibrary(People person) throws InterruptedException {
         person.library.showAllBooksInLibrary();
+        mainMenu(person);
+    }
+
+    public static void removeBookById(People person) throws  InterruptedException {
+        System.out.println("Enter the book ID that you want to remove: ");
+        Scanner sc = new Scanner(System.in);
+        int bookId = sc.nextInt();
+
+        person.library.removeBookById(bookId);
+        mainMenu(person);
+    }
+
+    public static void showPersonLibrary(People person) throws  InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input person's ID: ");
+        int personId = sc.nextInt();
+
+        person.library.showBooksPersonById(personId);
+
+        mainMenu(person);
+    }
+
+    public static void addBookToPersonLibrary(People person) throws  InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter person's ID: ");
+        int persId = sc.nextInt();
+
+        person.library.addBookToPersonLibraryById(persId);
+        mainMenu(person);
+    }
+
+    public static void setBookOnZeroById(People person) throws  InterruptedException {
+        person.library.setBookByIdOnZero();
         mainMenu(person);
     }
 }
